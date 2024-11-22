4065e74b70b01ba066c2a67c5d50a1cf2d5238a4
milk926 <nishana5148sherinov@gmail.com>
2024-11-22 10:50:01 +0530

    22/11/24

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:milkproject/user/page/login_page.dart';
 //import 'package:milkproject/user/page/login_page.dart';
-import 'package:milkproject/user/page/signup_page.dart';
-import 'package:milkproject/user/page/login_page.dart';
+//import 'package:milkproject/user/page/signup_page.dart';
+//import 'package:milkproject/user/page/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
